840d8a45f9986f045253178afa337da8fd039cbc
Suha Jameel <115213075+SuhaJem@users.noreply.github.com>
2025-04-06 23:29:12 +0530

    Create Hero.tsx

---
+++ components/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <section className="text-center mb-8">
+      <h1 className="text-5xl font-bold mb-4">Stat Quo</h1>
+      <p className="text-lg">Smart. Sleek. Unstoppable.</p>
+    </section>
+  );
+}
