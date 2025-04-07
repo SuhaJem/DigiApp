7614d3d9be282958d37d3736112a7da5a502fbc3
Suha Jameel <115213075+SuhaJem@users.noreply.github.com>
2025-04-07 07:35:34 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import './globals.css'
+import { ReactNode } from 'react'
+
+export const metadata = {
+  title: 'Stat Quo',
+  description: 'Tech that moves. Stay ahead.',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
