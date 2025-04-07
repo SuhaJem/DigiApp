1d5c1366ac49c425e6bd2c7a9f16c74c7e495a04
Suha Jameel <115213075+SuhaJem@users.noreply.github.com>
2025-04-07 07:25:38 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,11 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        electricBlue: '#00FFFF', // You can change this to a richer blue if you want
+        electricBlue: '#00FFFF', // or your preferred electric blue hex
       },
     },
   },
